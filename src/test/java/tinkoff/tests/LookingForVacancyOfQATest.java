ed4a9d1bf13d6402f623c3a499e32173ba7ab500
Toalra <K0$k@$1mk@2950>
2023-09-18 23:29:10 +0300

    tinkoff

--- src/test/java/tinkoff/tests/LookingForVacancyOfQATest.java
+++ src/test/java/tinkoff/tests/LookingForVacancyOfQATest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -29,5 +30,12 @@ public class LookingForVacancyOfQATest extends TestBase {
         $(byText("Java")).click();
 
         $(byText("Java разработчик в Банкоматы")).shouldHave(text("Java разработчик в Банкоматы"));
+
+    }
+    @Test
+    @Tag("tinkoff_articles")
+    public void articlesOpen() {
+        $("[data-schema-path='slides.0']").click();
+        $("[role='heading']").shouldHave(text("Как писать в резюме об опыте работы и навыках"));
     }
 }
