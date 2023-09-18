e108e80b9fb7ab53a2bbcea4b9a1c482110df7e4
Toalra <K0$k@$1mk@2950>
2023-09-18 23:41:29 +0300

    tinkoff

--- src/test/java/tinkoff/tests/LookingForVacancyOfQATest.java
+++ src/test/java/tinkoff/tests/LookingForVacancyOfQATest.java
@@ -33,6 +33,7 @@ public class LookingForVacancyOfQATest extends TestBase {
 
     }
     @Test
+    @Tag("tinkoff_articles")
     public void articlesOpen() {
         $("[data-schema-path='slides.0']").click();
         $("[role='heading']").shouldHave(text("Как писать в резюме об опыте работы и навыках"));
