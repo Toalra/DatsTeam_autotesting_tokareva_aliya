50d28d71f48852f6ae80397eb3bc83ba1468ce13
Toalra <K0$k@$1mk@2950>
2023-09-18 22:44:17 +0300

    tinkoff

--- src/test/java/tinkoff/tests/LookingForVacancyOfQATest.java
+++ src/test/java/tinkoff/tests/LookingForVacancyOfQATest.java
@@ -1,26 +1,33 @@
 package tinkoff.tests;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class LookingForVacancyOfQATest extends TestBase {
-//    @BeforeAll
-//    public void openWeb() {
-//        open("https://www.tinkoff.ru/career/it/about/");
-//    }
+    @BeforeAll
+    static void openWeb() {
+       open("https://www.tinkoff.ru/career/it/about/");
+    }
 
     @Test
     @Tag("tinkoff")
     public void lookingVacancyTest() {
-        $(".gbtjzKzLV").click();
-        $(".dbVmwKQGC").click();
-        $(".bIy-+Id").shouldHave(text("QA - инженер (frontend) в Тинькофф Инвестиции"));
-        $(".bIy-+Id").shouldHave(text("QA-инженер на backend приложений"));
-        $(".bIy-+Id").shouldHave(text("Инженер по тестированию веб-приложений"));
-        $(".bIy-+Id").shouldHave(text("QA-инженер в мобильное тестирование"));
-        $(".bIy-+Id").shouldHave(text("Full-stack QA (Java) в Тинькофф Страхование"));
+        $(byText("Работа в ИТ")).click();
+        $(byText("Смотреть вакансии")).click();
+        $(byText("Направление")).click();
+        $(byText("Backend")).click();
+        $(byText("Любой")).click();
+        $(byText("Middle")).click();
+        $(byText("Город")).click();
+        $(byText("Алматы")).click();
+        $(byText("Java")).click();
+
+        $(byText("Java-разработчик")).shouldHave(text("Java-разработчик "));
     }
 }
