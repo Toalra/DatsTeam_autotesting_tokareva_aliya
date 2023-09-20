77d74d98bffc684d0288aa7af3adb9735d6812be
Toalra <K0$k@$1mk@2950>
2023-09-20 22:40:37 +0300

    dats

---
+++ src/test/java/first_tests/tests/LookingForVacancyOfQATest.java
@@ -0,0 +1,38 @@
+package first_tests.tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+@Tag("test_1")
+public class LookingForVacancyOfQATest extends TestBase {
+    @BeforeAll
+    static void openWeb() {
+       open("https://dats.team/");
+    }
+
+    @Test
+    public void lookingVacancyTest() {
+        open();
+        $(".navbar-header").$(".trans-header__menu_1").click();
+        $("li[name='performances']").shouldHave(text("Всегда в тренде"));
+    }
+    @Test
+    public void chooseAnyOffersTests () {
+        open();
+        $(".header").$(".trans-header__menu_2").click();
+        $(".develop__first-section__intro__title").shouldHave(text("Про разработку"));
+
+    }
+    @Test
+    public void articlesOpen() {
+        $(".header").$(".trans-header__menu_3").click();
+        $(".PromoScreen_title__MT_n6").shouldHave(text("HYPERCORE FACTORY"));
+    }
+
+}
--- src/test/java/first_tests/tests/TestBase.java
+++ src/test/java/first_tests/tests/TestBase.java
@@ -1,4 +1,4 @@
-package tinkoff.tests;
+package first_tests.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -17,8 +17,8 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://dats.team/";
-        Configuration.browserSize = "1920x1080";
+//        Configuration.baseUrl = "https://dats.team/";
+//        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy="eager";
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
